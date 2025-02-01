c15226bf786e2b9ce9c2d50cd8624f3a1e9b817a
Chang Hung <zero102x@gmail.com>
2025-02-02 03:26:58 +0800

    feat: Add useHead in app.vue

--- app.vue
+++ app.vue
@@ -2,6 +2,19 @@
 const title = 'Jira'
 const description = 'Jira 首頁'
 
+useHead({
+  meta: [
+    { charset: 'utf-8' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+  ],
+  link: [
+    { rel: 'icon', href: `${config.app.baseURL}favicon.ico` }
+  ],
+  htmlAttrs: {
+    lang: 'en'
+  }
+})
+
 useSeoMeta({
   title,
   description,
